4a42321ef8e24942b1fddb36a213cde5308c1e73
durga <durgachary9967@gmail.com>
2025-02-06 17:30:07 +0530

    commit

--- toolstest/src/com/nit/cal/Calculation.java
+++ toolstest/src/com/nit/cal/Calculation.java
@@ -4,9 +4,10 @@ public class Calculation {
 	public static void main(String[] args) {
 		int n = 12;
 		int m = 10;
+		
 		for(int i=0;i<10;i++)
 		{
-			System.out.println(i);
+			System.out.println(i + "hello");
 		}
  	}
 
